45835af5926bcac22df765d2ffcec464a81dae3f
Luciana <52220617+lucianamerege@users.noreply.github.com>
2021-11-23 23:43:57 -0300

    Update laboratorio1.c

--- laboratorio1.c
+++ laboratorio1.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 
 #define NTHREADS 2 //definindo uma constante que vai ser o numero de threads
 #define TAM 10000
-int posicao = 0;
+
+//cria a estrutura de dados para armazenar os argumentos da thread
+typedef struct {
+   int *elementos, inicio;
+} t_Args;
 
 void * tarefa (void * arg){
-    int *valor = (int *) arg;
-
-    posicao = posicao+1;
+    t_Args *valor = (t_Args *) arg;
 
     for(int i=0; i<TAM/2; i++){
-        if(posicao == 1){ //isso checa se é a primeira ou a segunda metade do vetor
-            valor[i] = valor[i] * valor[i];
-        }else if(posicao == 2){
-            valor[i+(TAM/2)] = valor[i+(TAM/2)] * valor[i+(TAM/2)];
-        }
+        valor->elementos[i + valor->inicio] = (i + valor->inicio) * (i + valor->inicio);
     }
 
     pthread_exit(NULL); //retorno vazio
@@ -26,12 +25,21 @@ int main(void){
 
     int elementos[TAM], temp;
 
+    t_Args *arg;//receberá os argumentos para a thread
+
     for(int i=0; i<TAM; i++){
         elementos[i] = i;
     }
 
     for(int i = 0; i < NTHREADS; i++){
-        if (pthread_create(&tid[i], NULL, tarefa, (void *)elementos)){ 
+        arg = malloc(sizeof(t_Args));
+        if (arg == NULL) {
+            printf("--ERRO: malloc()\n"); exit(-1);
+        }
+        arg->elementos = elementos; 
+        arg->inicio = i * TAM/2; 
+
+        if (pthread_create(&tid[i], NULL, tarefa, (void *)arg)){ 
             printf("Erro.");
         }
     }
@@ -43,12 +51,13 @@ int main(void){
     } //esperar todas as threads terminarem antes de continuar a main
 
     for(int i=0; i<TAM; i++){ //estou checando se cada valor é igual ao valor inicial^2. Como os valores são iguais a sua posição no array, só criei essa variavel temporaria para guardar a posição do array ao quadrado e comparar com o valor naquela posição.
+        
         temp = i*i;
         if(elementos[i]!=temp){
-            printf("Elemento na posição %d está errado.", i);
+            printf("Elemento na posição %d está errado.\n", i);
         }
     }
 
-    printf("\nFim.\n");
+    printf("Fim.\n");
     return 0;
 }
